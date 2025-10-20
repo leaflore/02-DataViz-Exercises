3f76bc4d5188b9f589cd84fc01941e460bfaca85
leaflore <br.kemp1@gmail.com>
2025-10-19 18:03:59 -0700

    20251019 - Update project

--- project-folder/rollup.config.mjs
+++ project-folder/rollup.config.mjs
@@ -1,7 +1,46 @@
+import resolve from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+
 export default {
-    input: 'src/index.js',
+    // onwarn: function ( message ) {
+    // if (message.code === 'CIRCULAR_DEPENDENCY') {
+    //   return;
+    // }
+    // console.error(message);
+    // },
+    input:'./main.js',
     output: {
-        file: 'bundle.js',
-        format: 'cjs'
-    }
+      file:'build/bundle.js',
+      format:'iife',
+      name: 'MyReactApp',
+      globals: {
+        'd3': 'd3',
+      }
+    },
+    external: [
+      'd3',
+    ],
+    plugins: [
+      resolve(),
+      nodeResolve({
+          jsnext:true,
+          main:true,
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development'),
+        preventAssignment: true,
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        extensions: ['.js', '.jsx']
+      }),
+      commonjs({
+        include: 'node_modules/**'
+      }),
+      nodePolyfills()
+    ]
 };
--- project-folder/src/index.js
+++ project-folder/src/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import * as d3 from 'd3';
+import { csv } from 'd3';
 
 const csvUrl = 'https://gist.githubusercontent.com/leaflore/ae12e6178a767dc2d02f7a81ccd5cd4e/raw/BK-cssNamedColors.csv';
 
@@ -11,16 +11,17 @@ const height = 500;
 const App = () => {
     const [data, setData] = useState(null);
 
-    d3.csv(csvUrl).then(data => {
-        setData(data);
-        // let message = '';
-        // message = message + Math.round(d3.csvFormat(data).length / 10124) + 'kB\n';
-        // message = message + data.length + 'rows\n';
-        // message = message + data.columns.length + 'columns';
+    useEffect(() => {
+        csv(csvUrl).then(setData);
+    }, []);
 
-    })
+    if (!data) {
+        return <pre>Loading...</pre>
+    }
 
-    return <div>Data is : {data}</div>;
+    console.log(data[0]);
+
+    return data.map(d => <div style={{backgroundColor : d.color}}></div>);
 };
 
 const rootElement = document.getElementById('root');
